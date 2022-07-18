bb792ef54bee352a12487cb8ef9e30edb1710ad3
yangfan_c <yangfan_c@aspirecn.com>
2022-07-18 10:44:06 +0800

    mod: add user cqq/Token support

--- demo/vpork.py
+++ demo/vpork.py
@@ -16,6 +16,7 @@ from utils.config import Config
 class vpork:
     config = utils.config.Config.readJson()
     config_name = "vpork"  ## 对应config配置的key
+    config_domain = "https://forever.pork16.com"
     list = config['users'][config_name] if ('users' in config and config_name in config['users']) else []
     cqq = config['qqpush']['qq'] if (
             'qqpush' in config and 'qq' in config['qqpush']) else ''  # 请到 https://wx.scjtqs.com/qq 里面开启，访问需要先登录
@@ -29,7 +30,7 @@ class vpork:
             if 'email' not in account or 'passwd' not in account:
                 continue
             ## 登录
-            url = "https://forever.pork16.com/auth/login"
+            url = self.config_domain + "/auth/login"
             postdata = urllib.parse.urlencode({
                 'email': account['email'],
                 'passwd': account['passwd']
@@ -37,6 +38,10 @@ class vpork:
             header = {
                 "User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:32.0) Gecko/20100101 Firefox/32.0"
             }
+
+            userCqq = account['cqq'] if ('cqq' in account) else self.cqq
+            userToken = account['token'] if ('token' in account) else self.token
+
             req = urllib.request.Request(url, postdata, header)
             # 自动记住cookie
             cj = http.cookiejar.CookieJar()
@@ -46,12 +51,12 @@ class vpork:
             logincheck = json.loads(response)
             if logincheck['ret'] != 1:
                 print('登录失败')
-                ret = push.push(self.cqq, self.token, '账号' + account['email'] + '签到vpork登录失败')
+                ret = push.push(userCqq, useToken, '账号' + account['email'] + '签到vpork登录失败')
                 print(ret)
                 continue
             print('账号' + account['email'] + '登录成功')
             ## 签到
-            url = "https://forever.pork16.com/user/checkin"
+            url = url = self.config_domain + "/user/checkin"
             postdata = urllib.parse.urlencode({}).encode('utf-8')
             header = {
                 "User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:32.0) Gecko/20100101 Firefox/32.0"
@@ -65,17 +70,17 @@ class vpork:
                 print(ret)
             except ValueError:
                 print('签到失败')
-                push.push(self.cqq, self.token, '账号' + account['email'] + '签到vpork失败 msg:' + ret['msg'])
+                push.push(userCqq, useToken, '账号' + account['email'] + '签到vpork失败 msg:' + ret['msg'])
                 print(ret)
                 continue
             if ret['ret'] != 1:
                 print('签到失败')
-                ret = push.push(self.cqq, self.token, '账号' + account['email'] + '签到vpork失败 msg:' + ret['msg'])
+                ret = push.push(userCqq, useToken, '账号' + account['email'] + '签到vpork失败 msg:' + ret['msg'])
                 print(ret)
                 continue
             print(ret)
             # 登出
-            url = "https://forever.pork16.com/user/logout"
+            url = url = self.config_domain + "/user/logout"
             header = {
                 "User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:32.0) Gecko/20100101 Firefox/32.0",
             }
@@ -83,11 +88,11 @@ class vpork:
             opener = urllib.request.build_opener(urllib.request.HTTPCookieProcessor(cj))
             r = opener.open(req)
             response = r.read().decode('utf-8')
-            if self.cqq == '' or self.token == '':
+            if userCqq == '' or userToken == '':
                 logging.info('账号' + account['email'] + '签到vpork成功' + ',' + ret['msg'] + ',剩余流量' + ret['trafficInfo'][
                     'unUsedTraffic'])
             else:
-                push.push(self.cqq, self.token,
+                push.push(userCqq, useToken, 
                           '账号' + account['email'] + '签到vpork成功' + ',' + ret['msg'] + ',剩余流量' + ret['trafficInfo'][
                               'unUsedTraffic'])
         return True
